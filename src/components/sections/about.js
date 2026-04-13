b6f30f122081118d551b19ca1d61650c58d38cd1
SaqlainKL <saqlain.ali@koderlabs.com>
2026-04-14 00:04:37 +0500

    Updated portfolio

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef } from 'react';
-import { StaticImage } from 'gatsby-plugin-image';
 import styled from 'styled-components';
 import { srConfig } from '@config';
 import sr from '@utils/sr';
